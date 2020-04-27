24eaaf07ac52de1bfa49d7facd1726df54a704b0
Charles Rowland <rowland.charles@gmail.com>
2020-04-27 14:10:34 -0600

    added submit button and partial submit function

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
-import { View, Text } from "react-native";
-import { getMetricMetaInfo} from "../utils/helpers";
-import UdaciSlide from './UdaciSlider';
-import UdaciStepper from './UdaciSlider';
-import UdaciSlider from "./UdaciStepper";
+import { View, Text, TouchableOpacity } from "react-native";
+import { getMetricMetaInfo,  timeToString } from "../utils/helpers";
+import UdaciSlider from './UdaciSlider';
+import UdaciStepper from "./UdaciStepper";
+import DateHeader from "./DateHeader";
+
+function SubmitBtn({ onPress }) {
+    return (
+        <TouchableOpacity
+            onPress={onPress}>
+            <Text>Submit</Text>
+        </TouchableOpacity>
+    )
+}
 
 export default class AddEntry extends Component {
     // metrics we are tracking
@@ -47,11 +56,26 @@ export default class AddEntry extends Component {
         }))
     }
 
+    submit = () => {
+        const key = timeToString();
+        const entry = this.state;
+
+        //update state
+        this.setState(() => ({
+                run: 0,
+                bike: 0,
+                swim: 0,
+                sleep: 0,
+                eat: 0,
+        }))
+    }
+
     render() {
         const metaInfo = getMetricMetaInfo();
 
         return (
             <View>
+                <DateHeader date={(new Date()).toLocaleDateString()} />
                 {Object.keys(metaInfo).map((key) => {
                     const { getIcon, type, ...rest } = metaInfo[key];
                     const value = this.state[key];
@@ -75,6 +99,8 @@ export default class AddEntry extends Component {
                         </View>
                     )
                 })}
+
+                <SubmitBtn onPress={this.submit} />
             </View>
         );
     }
---
+++ components/DateHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+const DateHeader = ({date}) => {
+    return (
+        <Text>
+            {date}
+        </Text>
+    );
+};
+
+export default DateHeader;
