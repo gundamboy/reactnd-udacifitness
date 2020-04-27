7c11191f94d00044c5d175433ac3228a83a82328
Charles Rowland <rowland.charles@gmail.com>
2020-04-27 14:47:18 -0600

    added stepper buttons to ui

--- components/UdaciStepper.js
+++ components/UdaciStepper.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { FontAwesome, Entype } from '@expo/vector-icons';
 
-const UdaciStepper = () => {
+const UdaciStepper = ({ max, unit, step, value, onIncrement, onDecrement }) => {
     return (
         <View>
-            <Text>UdaciStepper</Text>
+            <View>
+                <TouchableOpacity onPress={onDecrement}>
+                    <FontAwesome name={'minus'} size={30}  color={'black'} />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={onIncrement}>
+                    <FontAwesome name={'plus'} size={30}  color={'black'} />
+                </TouchableOpacity>
+            </View>
+
+            <View>
+                <Text>{value}</Text>
+                <Text>{unit}</Text>
+            </View>
         </View>
     );
 };
