70d7052da198697852c91ea2df9695c61ab2d05c
Charles Rowland <rowland.charles@gmail.com>
2020-04-30 13:13:04 -0600

    added history component and calendar

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
 import { AsyncStorage } from 'react-native';
-import { CALENDAR_STORAGE_KEY } from './_calendar';
+import { CALENDAR_STORAGE_KEY, formatCalendarResults } from './_calendar';
 
 export function submitEntry({ key, entry }) {
     return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({
@@ -16,4 +16,8 @@ export function removeEntry({ key }) {
 
             AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
         })
+}
+
+export function fetchCalendarResults() {
+    return AsyncStorage.getItem(CALENDAR_STORAGE_KEY).then(formatCalendarResults);
 }
